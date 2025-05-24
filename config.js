056df208e1d756561a2d5ac4ba1db75a546bd41a
satvika harivanam <sathvikaharivanam@gmail.com>
2025-05-25 01:47:41 +0530

    merging

--- config.js
+++ config.js
@@ -5,11 +5,7 @@ module.exports = {
   mongoURI: 'mongodb://localhost:27017/studentdatabase',
 
   // Server settings
-<<<<<<< HEAD
   port: 5001,
-=======
-  port: 5000,
->>>>>>> ffcf602b7c0ea04ae2dee5c63b2dd299433a9ed1
 
   // File paths
   uploadDir: __dirname + '/uploads',
